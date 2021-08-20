108299dd8512d7701205a8cd2aad7536642db8b9
Jeremy Grecu <jeremy.grecu@sap.com>
2021-08-20 14:55:59 +0100

    JBA - Tic-Tac-Toe with AI - Stage 3/5: Watch 'em fight -- finish

--- task/src/tictactoe/Game.java
+++ task/src/tictactoe/Game.java
@@ -3,10 +3,22 @@ package tictactoe;
 import java.util.Scanner;
 
 public class Game {
+    private static final String BAD_PARAMETERS = "Bad parameters!";
     private final Scanner scanner = new Scanner(System.in);
     private final TicTacToeField field = new TicTacToeField();
+    private final String[] commands = new String[3];
+    private boolean userTurn;
+    private boolean justComputer = false;
 
     public void play() {
+        while (true) {
+            System.out.println("Input command: ");
+            String string = scanner.nextLine();
+            if (checkInput(string)) {
+                userTurn = "user".equals(commands[1]);
+                break;
+            }
+        }
         field.printBoard();
         startGame();
     }
@@ -22,12 +34,11 @@ public class Game {
     }
 
     private void startGame() {
-        int player = 0;
         while (field.getGameState() == GameState.NOT_FINISHED) {
-            if (player % 2 == 0) {
-                getCoordinates();
-            } else {
+            if (justComputer) {
                 field.easyAiMove();
+            } else {
+                playUser();
             }
             field.printBoard();
 
@@ -39,9 +50,46 @@ public class Game {
                 System.out.println("Draw");
                 scanner.close();
             }
-            player++;
         }
     }
 
+    private boolean checkInput(String input) {
+        String[] parts = input.toLowerCase().split("\\s+");
+        if ("exit".equals(parts[0])) {
+            System.exit(0);
+        }
+        if (parts.length != 3 ) {
+            System.out.println(BAD_PARAMETERS);
+            return false;
+        }
+        if (!"start".equals(parts[0])) {
+            System.out.println(BAD_PARAMETERS);
+            return false;
+        }
+        if ("easy".equals(parts[1]) && "user".equals(parts[2]) || "user".equals(parts[1]) && "easy".equals(parts[2])) {
+            this.commands[0] = parts[0];
+            this.commands[1] = parts[1];
+            this.commands[2] = parts[2];
+            return true;
+        }
+        if ("easy".equals(parts[1]) && "easy".equals(parts[2])) {
+            this.commands[0] = parts[0];
+            this.commands[1] = parts[1];
+            this.commands[2] = parts[2];
+            this.justComputer = true;
+            return true;
+        }
+        System.out.println(BAD_PARAMETERS);
+        return false;
+    }
+
+    private void playUser() {
+        if (userTurn) {
+            getCoordinates();
+        } else {
+            field.easyAiMove();
+        }
+        userTurn = !userTurn;
+    }
 
 }
